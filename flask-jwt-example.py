85ed0574037c0349991f85ee6535d56d2c363859
Rohit Chormale <rohitchormale@gmail.com>
2019-02-06 12:56:26 +0530

    Updated flask-jwt-example with commits

--- flask-jwt-example.py
+++ flask-jwt-example.py
@@ -1,22 +1,31 @@
 """
-flask-jwt example
+This small script demonstrate example of JWT authentication using flask-jwt extension.
+Most of code is copied from links in references.
 
-# Usage
+Requirements:
+Click==7.0
+Flask==1.0.2
+Flask-JWT==0.3.2
+itsdangerous==1.1.0
+Jinja2==2.10
+MarkupSafe==1.1.0
+PyJWT==1.4.2
+Werkzeug==0.14.1
 
-- Run script 
 
-python flask-jwt-example.py
+Usage:
+To use flask-jwt, 2 functions needs to define
+1. authenticate(username, password)
+2. identity(payload)
 
+To run script 'python flask-jwt-example.py'
 
-- get jwt token
-
+To get jwt token send POST request to url '/api/auth/get_token' (configured in ConfigClass)
 url: /api/auth/get_token
 method: POST
 body: {"username": <username>, "password": <password>}
 
-- use token
-
-Add 'authorization' header with value as 'JWT <auth-token>'
+To use JWT token, add 'authorization' header with token value along with prefix 'JWT'
 e.g.
 url: /home
 method: GET
@@ -27,11 +36,13 @@ headers: authorization: JWT 1313asfasdfsd13113assdf
 - https://pythonhosted.org/Flask-JWT/
 
 """
+from werkzeug.security import safe_str_cmp
 from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
 
 
 class User(object):
+    """Example User class"""
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
@@ -40,38 +51,49 @@ class User(object):
     def __str__(self):
         return "User(id=%s)" % self.id
 
+
+# Instead of actual database, we are using here python variables to store data
 users = [
     User(1, 'user1', 'user1'),
     User(2, 'user2', 'user2'),
 ]
-
 username_table = {u.username: u for u in users}
-userid_table =  {u.id: u for u in users}
+userid_table = {u.id: u for u in users}
 
-from werkzeug.security import safe_str_cmp
+
+# implementing required functions
 def authenticate(username, password):
+    """Authenticate user and return User instance based on username and password"""
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
 
+
 def identity(payload):
+    """Return User instance or None based on token"""
     user_id = payload['identity']
     return userid_table.get(user_id, None)
 
+
 class ConfigClass(object):
+    """Test Configuration"""
+    SECRET_KEY = "my-secret-key"
     JWT_AUTH_URL_RULE = "/api/auth/get_token"
 
+
+# initialize app and JWT
 app = Flask(__name__)
 app.debug = True
-app.config["SECRET_KEY"] = "my-secret-key"
 app.config.from_object(ConfigClass)
 jwt = JWT(app, authenticate, identity)
 
 
+# protecting view
 @app.route("/home")
 @jwt_required()
 def home():
     return 'home'
+
 
 if __name__ == "__main__":
     app.run()
