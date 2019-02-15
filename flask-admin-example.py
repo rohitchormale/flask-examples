afcd8a8a1ec2784f4db11ebc7b4c11aaa8b1c5b0
Rohit Chormale <rohitchormale@gmail.com>
2019-02-15 16:20:11 +0530

    Updated flask-admin example

--- flask-admin-example.py
+++ flask-admin-example.py
@@ -1,4 +1,5 @@
 """
+flask-admin-example.py
 
 Requirements:
 Click==7.0
@@ -13,7 +14,8 @@ Werkzeug==0.14.1
 WTForms==2.2.1
 
 Usage:
-python flask-admin-example.py
+ - Run script using 'python flask-admin-example.py'
+ - Then visit 'http://127.0.0.1:5555/admin'
 
 References:
 - https://flask-admin.readthedocs.io/en/latest/
@@ -27,6 +29,30 @@ from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+# database models
+from sqlalchemy.sql import func
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, server_default=func.now())
+    date_modified = db.Column(db.DateTime, onupdate=func.now())
+    username = db.Column(db.String(32), nullable=False, unique=True)
+    posts = db.relationship("Post", backref="user", lazy=True)
+
+    def __repr__(self):
+        return "<%s>" % self.username
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, server_default=func.now())
+    date_modified = db.Column(db.DateTime, onupdate=func.now())
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    title = db.Column(db.String(32), nullable=False)
+    text = db.Column(db.Text)
+
+    def __repr__(self):
+        return "<%s>" % self.title
+
 
 def create_app():
     # init flask app
@@ -38,35 +64,12 @@ def create_app():
     db.init_app(app)
 
     with app.app_context():
-        # database models
-        from sqlalchemy.sql import func
-        class User(db.Model):
-            id = db.Column(db.Integer, primary_key=True)
-            date_created = db.Column(db.DateTime, server_default=func.now())
-            date_modified = db.Column(db.DateTime, onupdate=func.now())
-            username = db.Column(db.String(32), nullable=False, unique=True)
-            posts = db.relationship("Post", backref="user", lazy=True)
-
-            def __repr__(self):
-                return "<%s>" % self.username
-
-        class Post(db.Model):
-            id = db.Column(db.Integer, primary_key=True)
-            date_created = db.Column(db.DateTime, server_default=func.now())
-            date_modified = db.Column(db.DateTime, onupdate=func.now())
-            author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-            title = db.Column(db.String(32), nullable=False)
-            text = db.Column(db.Text)
-
-            def __repr__(self):
-                return "<%s>" % self.title
-
         # init admin
         admin = Admin(app, name="microblog", template_mode="bootstrap3")
         admin.add_view(ModelView(User, db.session))
         admin.add_view(ModelView(Post, db.session))
 
-        # init database
+        # create tables
         db.create_all()
 
     return app
